13e2c5680535d8353b58e3c49802b5b697758587
Jake P <93plaistj@protonmail.ch>
2018-07-03 17:26:20 -0400

    click event added, js almost finished

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    var wins = 0, losses = 0;
+    var wins = 0, losses = 0, usrTotal = 0;
 
     //gets random sum between 15 & 65 --> appends getSum to HTML
     var getSum = Math.floor(Math.random() * 50+15);
@@ -8,29 +8,64 @@ $(document).ready(function(){
     console.log(getSum + ' sum');
     
     //List of OSRS gems, uses gemSum to find random number
-    var ruby, diamond, dragonstone;
+    var ruby, diamond, dragonstone, onyx;
     var gemNum = function(){
         return Math.floor(Math.random() * 10 + 5);
     }
     console.log('Ruby Val: ' + gemNum(ruby) + ' - Diamond Val: ' + gemNum(diamond)
-        + ' - DragonStone Val: ' + gemNum(dragonstone));
+        + ' - DragonStone Val: ' + gemNum(dragonstone) + ' - Onyx Val: ' + gemNum(onyx));
 
+
+    ruby = ruby[gemNum()]
     //appending variables to HTML
-    $('#Sum').text(getSum);
-    $('wins').text(wins);
-    $('loss').text(losses);
+    $('#sum').text(getSum);
+    $('#wins').text(wins);
+    $('#loss').text(losses);
 
-    $('ruby').text(ruby);
-    $('diamond').text(ruby);
-    $('dragonstone').text(dragonstone);
+    $('#ruby').text(ruby);
+    $('#diamond').text(ruby);
+    $('#dragonstone').text(dragonstone);
 
+    $('#total').text(usrTotal);
+
+
+    //Function to reset the game, Getting new Values, and 're-Appending'
     gameReset = function(){
-        getSum;
+
+        usrTotal = 0;
+        getSum = Math.floor(Math.random() * 50+15);
+        /*gemNum(ruby);
+        gemNum(diamond);
+        gemNum(dragonstone);
+        gemNum(onyx);*/
+
         console.log('Sum to Acheive: ' + getSum);
-        
         console.log('Ruby Val: ' + gemNum(ruby) + ' - Diamond Val: ' + gemNum(diamond)
-        + ' - DragonStone Val: ' + gemNum(dragonstone));
+        + ' - DragonStone Val: ' + gemNum(dragonstone) + ' - Onyx Val: ' + gemNum(onyx));
         
+        $('#sum').text(getSum);
+        $('#total').text(usrTotal)
     }
-    gameReset();
+   // gameReset();
+
+    //sets up the on click event for gem 'buttons'
+
+    $('#gemOne').on('click', function(){
+        usrTotal += gemNum(ruby);
+        console.log('New total: ' + usrTotal);
+        console.log(gemNum(ruby))
+    });
+    $('#gemTwo').on('click', function(){
+        usrTotal += gemNum(diamond);
+        console.log('New total: ' + usrTotal);
+    })
+    $('gemThree').on('click', function(){
+        usrTotal += gemNum(dragonstone);
+        console.log('New total: ' + usrTotal);
+    })
+    $('gemFour').on('click', function(){
+        usrTotal += gemNum(onyx);
+        console.log('New total: ' + usrTotal);
+    })
+
 });
